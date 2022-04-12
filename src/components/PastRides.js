244664d690e46ee6fed8702f40a4a1563523714e
riteshahir <61563106+riteshahir@users.noreply.github.com>
2022-04-12 13:09:48 +0530

    Update PastRides.js

--- src/components/PastRides.js
+++ src/components/PastRides.js
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function PastRides() {
-  return <div className="card">
-  <div className="card-img">
-    <img src="https://picsum.photos/200" alt="card-img" />
-  </div>
-  <div className="card-detail">
-    <p>Ride Id:</p>
-    <p>Origin Station:</p>
-    <p>station_path:</p>
-    <p>Date:</p>
-    <p>Distance:</p>
-  </div>
-  <div className="card-place">
-    <p>city name</p>
-    <p>state name</p>
-  </div>
-</div>;
+
+  const [pastRides,setPastRides]= useState([]);
+
+  useEffect(() => {
+    fetchData();
+  },[]);
+  
+
+  const date=new Date();
+  // console.log(date);
+
+  async function fetchData(){
+    await fetch("https://assessment.api.vweb.app/rides").then(res => {
+      return res.json();
+    }).then(rides => {
+      return setPastRides(rides);
+    }).catch(err =>{
+      console.log(err);
+    })
+
+    pastRides.filter((element,index) => {
+      return element.date < date;
+    })
+
+  }
+
+  function formStationPath(station_path) {
+    let ans='[';
+    station_path.forEach((element,index) => {
+      ans+=element;
+      if(index!==station_path.length-1)
+        ans+=", ";
+    });
+    ans+=']';
+    return ans;
+  }
+
+  return (
+    pastRides && 
+    pastRides.map((item, i) => {
+      return (
+        <div className="card" key={i}>
+          <div className="card-img">
+            <img src={item.map_url} alt="card-img" />
+          </div>
+          <div className="card-detail">
+            <p>Ride Id: {item.id}</p>
+            <p>Origin Station: {item.origin_station_code}</p>
+            <p>station_path: {formStationPath(item.station_path)}</p>
+            <p>Date: {item.date}</p>
+            <p>Distance: {item.distance}</p>
+          </div>
+          <div className="card-place">
+            <p>{item.city}</p>
+            <p>{item.state}</p>
+          </div>
+        </div>
+      );
+    })
+  )
 }
